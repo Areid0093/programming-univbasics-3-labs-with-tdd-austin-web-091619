956793b7890282e4e82cb880b5daa2829fe0c8bd
Alec Reid <alecreid0093@gmail.com>
2019-09-14 22:23:32 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 # Add your variables here
 first_number = 10
 second_number = 5
-sum = (first_number + second_number)
+sum = (10 + 5)
 difference = (first_number + second_number)
 product = (first_number * second_number)
 quotient = (first_number / second_number)
